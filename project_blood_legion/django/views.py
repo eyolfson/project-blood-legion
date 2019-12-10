3e582512b2628d8a8795c713f76abb34d332c348
Jon Eyolfson <eyolfson@gmail.com>
2019-12-09 19:33:42 -0500

    Remove title from context and into templates

--- project_blood_legion/django/views.py
+++ project_blood_legion/django/views.py
@@ -7,16 +7,12 @@ from .forms import LootForm
 from .models import Character, Item, Raid
 
 def index(request):
-	context = {
-		'title': 'Blood Legion',
-	}
-	return render(request, 'project_blood_legion/index.html', context)
+	return render(request, 'project_blood_legion/index.html', {})
 
 @login_required
 @permission_required('project_blood_legion.view_character', raise_exception=True)
 def character_index(request):
 	context = {
-		'title': 'Characters',
 		'characters': Character.objects.order_by('cls', 'name'),
 	}
 	return render(request, 'project_blood_legion/character_index.html', context)
@@ -26,7 +22,6 @@ def character_index(request):
 def character_detail(request, character_id):
 	character = get_object_or_404(Character, pk=character_id)
 	context = {
-		'title': character.name,
 		'character': character,
 	}
 	return render(request, 'project_blood_legion/character_detail.html', context)
@@ -35,7 +30,6 @@ def character_detail(request, character_id):
 @permission_required('project_blood_legion.view_item', raise_exception=True)
 def item_index(request):
 	context = {
-		'title': 'Items',
 		'items': Item.objects.all(),
 	}
 	return render(request, 'project_blood_legion/item_index.html', context)
@@ -45,7 +39,6 @@ def item_index(request):
 def item_detail(request, item_id):
 	item = get_object_or_404(Item, pk=item_id)
 	context = {
-		'title': item.name,
 		'item': item,
 	}
 	return render(request, 'project_blood_legion/item_detail.html', context)
@@ -54,7 +47,6 @@ def item_detail(request, item_id):
 @permission_required('project_blood_legion.view_raid', raise_exception=True)
 def raid_index(request):
 	context = {
-		'title': 'Raids',
 		'raids': Raid.objects.all(),
 	}
 	return render(request, 'project_blood_legion/raid_index.html', context)
@@ -77,7 +69,6 @@ def raid_detail(request, raid_id):
 			else:
 				group.loot_form = LootForm(prefix=group.prefix)
 	context = {
-		'title': str(raid),
 		'raid': raid,
 		'groups': groups,
 	}
