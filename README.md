5e4168f5496a7f5f7688fddd3d74500826c5127d
Jon Eyolfson <eyolfson@gmail.com>
2019-12-14 15:31:38 -0500

    Added loot quality colors

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ A website for World of Warcraft Classic guilds
     source venv/bin/activate
     pip install --upgrade pip
     pip install requirements.txt
+    git submodule update --init --recursive
+    python -m project_blood_legion --update-css
     python manage.py migrate
     python manage.py loaddata initial_data
     python manage.py runserver
--- project_blood_legion/core/__init__.py
+++ project_blood_legion/core/__init__.py
@@ -7,11 +7,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 def update_css():
 	css_dir = os.path.join(BASE_DIR, 'css')
 	sass_path = os.path.join(css_dir, 'project-blood-legion.scss')
-	css_path = os.path.join(BASE_DIR, 'project_blood_legion', 'django',
-	                        'static', 'css', 'project-blood-legion.css')
-	subprocess.run(['sass', '--sourcemap=none', sass_path, css_path],
+	static_dir = os.path.join(BASE_DIR, 'project_blood_legion', 'django',
+	                          'static', 'css')
+	os.makedirs(static_dir, exist_ok=True)
+	static_path = os.path.join(static_dir, 'project-blood-legion.css')
+	subprocess.run(['sass', '--sourcemap=none', sass_path, static_path],
 	               cwd=css_dir)
-	print(BASE_DIR)
 
 def main(unparsed_args):
 	parser = argparse.ArgumentParser(prog='project_blood_legion')
