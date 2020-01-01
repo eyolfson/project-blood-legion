b8acaf95aed5853f27ad4ec5c4efa97a43b74f10
Jon Eyolfson <eyolfson@gmail.com>
2020-01-01 12:21:55 -0500

    Improve form and character display

--- website/settings.py
+++ website/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
 	'django.contrib.sites',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'django.forms',
 
 	'allauth',
 	'allauth.account',
@@ -50,6 +51,8 @@ TEMPLATES = [
 		},
 	},
 ]
+
+FORM_RENDERER = 'django.forms.renderers.TemplatesSetting'
 
 WSGI_APPLICATION = 'website.wsgi.application'
 
